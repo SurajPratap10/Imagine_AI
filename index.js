904aedc81fe07457626f96f558e2ce2aca67c0e1
sharmavikas4 <vikassharma20015@gmail.com>
2023-05-20 14:39:06 +0530

    Updated v2

--- index.js
+++ index.js
@@ -14,16 +14,9 @@ app.use(express.urlencoded({ extended: false }));
 
 
 
-//STATIC FOLDER: 
+//STATIC FOLDER:
 app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use('/openai', require('./routes/openaiRoutes'));
-app.listen(port, () => console.log(`Server started on port ${port}`));
-
-
-
-
-
-
-
+app.listen(process.env.PORT, () => console.log(`Server started on port ${port}`));
