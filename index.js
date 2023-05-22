2da5bc77980aaf44fee90e71728175f6cc05d03d
PranavSikarwal <sikarwalpranav123@gmail.com>
2023-05-22 19:11:23 +0530

    added viewRoutes and deleted index.html

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const path = require('path');
 const express = require('express');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
-
+const viewRoutes = require('./routes/viewRoutes');
 
 
 const app = express();
+
+//EJS AS RENDER ENGINE
+app.set('view engine', 'ejs');
+app.set('views','views'); //look for views in views folder
 
 //ENABLING BODY PARSER:
 app.use(express.json());
@@ -21,6 +25,8 @@ res.status(500).json({ error: 'Internal Server Error' })});
 //STATIC FOLDER:
 app.use(express.static(path.join(__dirname, 'public')));
 
+//View Routes taken from viewRoutes.js
+app.use(viewRoutes);
 
 app.use('/openai', require('./routes/openaiRoutes'));
 app.listen(process.env.PORT, () => console.log(`Server started on port ${port}`));
