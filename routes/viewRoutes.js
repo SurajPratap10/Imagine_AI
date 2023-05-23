af144019196aca34ad0a2d5cfcb0a7bf32bf9216
PranavSikarwal <sikarwalpranav123@gmail.com>
2023-05-23 21:48:50 +0530

    divided into ejs views

---
+++ routes/viewRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/',(_req,res,_next)=>{
+    res.render('home',{path: '/'}); // passed path to track active <a> class in header.ejs
+});
+
+router.get('/generateImg',(_req,res,_next)=>{
+    res.render('imagineAi',{path: '/generateImg'});
+});
+
+router.get('/contactUs',(_req,res,_next)=>{
+    res.render('contactUs',{path: '/contactUs'});
+});
+
+module.exports = router;
