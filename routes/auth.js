dc926e0fcd02aa67cb7689b57b1cee736b6c31e6
abhijeet <abhijeetnishal@gmail.com>
2023-05-31 21:15:02 +0530

    Added logout route with Functionality

--- routes/auth.js
+++ routes/auth.js
@@ -2,10 +2,12 @@ const express = require('express');
 const {
 	signupController,
 	loginController,
+	logoutController
 } = require('../controllers/authController');
 const router = express.Router();
 
 router.post('/signup', signupController);
 router.post('/login', loginController);
+router.post('/logout', logoutController);
 
 module.exports = router;
