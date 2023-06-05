eefc92775e444410e07ac906e567ef169347728a
Sameep002 <108282267+Sameep002@users.noreply.github.com>
2023-06-05 09:00:36 +0530

    Update main.js
    Signed-off-by: Sameep002 <108282267+Sameep002@users.noreply.github.com>

--- public/js/main.js
+++ public/js/main.js
@@ -189,3 +189,20 @@ const observer = new IntersectionObserver(animateCards, options);
 cards.forEach((card) => {
   observer.observe(card);
 });
+
+const gap = 16;
+
+const carousel = document.getElementById("carousel"),
+  content = document.getElementById("content"),
+  next = document.getElementById("next"),
+  prev = document.getElementById("prev");
+
+next.addEventListener("click", e => {
+  carousel.scrollBy(width + gap, 0);
+});
+prev.addEventListener("click", e => {
+  carousel.scrollBy(-(width + gap), 0);
+});
+
+let width = carousel.offsetWidth;
+window.addEventListener("resize", e => (width = carousel.offsetWidth));
