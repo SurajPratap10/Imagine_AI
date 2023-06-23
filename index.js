0ad5b1a4bacca9f326e1b8cc80052bdad3040739
sharmavikas4 <vikassharma20015@gmail.com>
2023-06-23 08:02:06 +0530

    test 2

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:5000/auth/google/callback",
+  callbackURL: "https://imagine-ai-1.vercel.app/auth/google/callback",
   userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
   scope: ["profile","email"]
 }, function(accessToken, refreshToken, profile, cb) {
