b249a73bf99990c6871249c67d05aa779d8504ae
SurajPratap10 <SurajPratap10@users.noreply.github.com>
2023-06-28 15:50:35 +0000

    Prettified Code!

--- public/js/validation.js
+++ public/js/validation.js
@@ -1,7 +1,7 @@
 const emailRegex =
   /^\w+([\.-]?\w+)*@(gmail\.com|yahoo\.com|hotmail\.com|aol\.com|outlook\.com)$/;
 const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[@$%#^&*])(?=.*[0-9]).{8,}$/;
-const phoneRegex =/^\d{10}$/;
+const phoneRegex = /^\d{10}$/;
 
 const validate = {
   name: (value) => {
@@ -14,19 +14,19 @@ const validate = {
   password: (value) => {
     return passwordRegex.test(value) ? false : true;
   },
-  FirstName: (value)=>{
+  FirstName: (value) => {
     return value.trim().length < 3 ? true : false;
   },
-  LastName :(value)=>{
+  LastName: (value) => {
     return value.trim().length < 3 ? true : false;
   },
-  PhoneNumber: (value)=>{
-    return phoneRegex.test(value)? false : true;
+  PhoneNumber: (value) => {
+    return phoneRegex.test(value) ? false : true;
   },
-  feedback: (value)=>{
+  feedback: (value) => {
     const words = value.trim().split(/\s+/).length;
-    return (words< 10 || words > 50) ?  true:  false
-  }
+    return words < 10 || words > 50 ? true : false;
+  },
 };
 
 function handleChange(e) {
@@ -40,28 +40,26 @@ function handleChange(e) {
   }
 }
 
-window.addEventListener("load", ()=>{
-// Contact form Submission validation
+window.addEventListener("load", () => {
+  // Contact form Submission validation
   const contactform = document.getElementById("contactform");
-  contactform.addEventListener("submit", (e)=>{
+  contactform.addEventListener("submit", (e) => {
     e.preventDefault();
     let submitable = true;
     const error = [...document.getElementsByClassName("contactError")];
-    console.log(error)
-    error.forEach(elem=>{
-      if(!elem.classList.contains("hidden")){
+    console.log(error);
+    error.forEach((elem) => {
+      if (!elem.classList.contains("hidden")) {
         submitable = false;
         return;
       }
-    })
-   
-    if(!submitable){
-        alert("Please enter valid values")
-    }else{
+    });
+
+    if (!submitable) {
+      alert("Please enter valid values");
+    } else {
       // Uncomment below line after implementaion of form submit handling
       // contactform.submit()
     }
-})
-  
-})
-
+  });
+});
