39063a9ef8165084cd367bb832238fac47020c84
SurajPratap10 <SurajPratap10@users.noreply.github.com>
2023-07-05 17:39:33 +0000

    Prettified Code!

--- public/js/main.js
+++ public/js/main.js
@@ -109,19 +109,19 @@ async function generateImageRequest(prompt, API_KEY, size) {
             {
               label: "Instagram",
               url: `https://www.instagram.com/create/collection/?source_url=${encodeURIComponent(
-                url
+                url,
               )}&media_type=gif`,
             },
             {
               label: "Facebook",
               url: `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(
-                url
+                url,
               )}`,
             },
             {
               label: "Twitter",
               url: `https://twitter.com/intent/tweet?text=${encodeURIComponent(
-                "Check out this GIF!"
+                "Check out this GIF!",
               )}&url=${encodeURIComponent(url)}`,
             },
             {
@@ -131,7 +131,7 @@ async function generateImageRequest(prompt, API_KEY, size) {
             {
               label: "Telegram",
               url: `https://telegram.me/share/url?url=${encodeURIComponent(
-                url
+                url,
               )}`,
             },
           ];
