f2f104b1da80f07734c35817a347e02bdb20a75e
AlfiyaSiddique <alfiyasiddique1708@gmail.com>
2023-07-09 23:13:20 +0530

    Bug Change

--- public/js/signup.js
+++ public/js/signup.js
@@ -14,17 +14,20 @@ generateCaptcha();
 async function checkCaptcha (){
   const captch = document.getElementById("captcha").innerText;
   const userCaptcha = document.querySelector("input[name='captcha']");
+  const captchaBtn = document.getElementById("captchabtn");
   if(captch === userCaptcha.value){
-    document.getElementById("captchabtn").innerText = "Captcha Verified";
-    document.getElementById("captchabtn").style.backgroundColor = "green";
+    captchaBtn.innerText = "Captcha Verified";
+    captchaBtn.style.backgroundColor = "green";
     document.getElementById("submit").disabled = false;
     document.querySelector("#submit span").style.cursor = "pointer";
   }else{
-    document.getElementById("captchabtn").innerText = " Invalid Captcha";
-    document.getElementById("captchabtn").style.backgroundColor = "red";
+    captchaBtn.innerText = " Invalid Captcha";
+    captchaBtn.style.backgroundColor = "red";
+
     setTimeout(()=>{
     generateCaptcha();
-    document.getElementById("captchabtn").style.backgroundColor = "black";
+    captchaBtn.style.backgroundColor = "black";
+    captchaBtn.innerText = "Verify Captcha"
     userCaptcha.value = ""
     }, 2000)
   }
