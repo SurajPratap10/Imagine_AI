b8feb24371a060958843967d3089561ff99617f5
haseebzaki-07 <haseebzaki13@gmail.com>
2024-10-21 15:23:36 +0530

    add subscribe

---
+++ routes/subscribe.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const { subscribeToEmail } = require('../controllers/subscribeController');
+
+const router = express.Router();
+
+router.post('/', subscribeToEmail);
+
+module.exports = router;
+
